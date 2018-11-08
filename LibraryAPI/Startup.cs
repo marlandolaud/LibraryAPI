d36a338904768714cf91906f6f7349a6397f08d1
mlaud <mlaud@outlook.com>
2018-11-07 19:26:43 -0500

    added global 500 server fault message

--- LibraryAPI/Startup.cs
+++ LibraryAPI/Startup.cs
@@ -3,6 +3,7 @@ using LibraryAPI.Helpers;
 using LibraryAPI.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -40,6 +41,17 @@ namespace LibraryAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context => 
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("An unexpected fault happened. Try again later");
+                    });
+                });
             }
 
             AutoMapper.Mapper.Initialize(authorMapper => {
