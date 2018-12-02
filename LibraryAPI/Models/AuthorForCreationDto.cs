00919cce063c11f1123c5dde53f94283e38c4a61
mlaud <mlaud@outlook.com>
2018-12-02 17:55:57 -0500

    added ability to initialize multiple child items with new parent item

--- LibraryAPI/Models/AuthorForCreationDto.cs
+++ LibraryAPI/Models/AuthorForCreationDto.cs
@@ -14,5 +14,8 @@ namespace LibraryAPI.Models
         public DateTimeOffset DateOfBirth { get; set; }
 
         public string Genre { get; set; }
+
+        public ICollection<BookForCreationDto> Books { get; set; } 
+            = new List<BookForCreationDto>();
     }
 }
