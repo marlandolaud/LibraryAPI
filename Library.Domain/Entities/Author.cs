d73d423c9fd855a975273b799650be34df5249e7
mlaud <mlaud@outlook.com>
2019-04-06 15:28:40 -0400

    separated repos and services to domain layer (not finished)

---
+++ Library.Domain/Entities/Author.cs
@@ -0,0 +1,31 @@
+﻿using Library.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Domain.Entities
+{
+    public class Author
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTimeOffset DateOfBirth { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Genre { get; set; }
+
+        public ICollection<Book> Books { get; set; }
+            = new List<Book>();
+    }
+}
