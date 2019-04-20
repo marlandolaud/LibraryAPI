72cfa78097beb5b53bd8d0122781dfc68c08bd70
mlaud <mlaud@outlook.com>
2019-04-20 10:05:55 -0400

    Fixed IEnumerableExtensions to use ExpandoObject from System.Dynamic namespace

--- Library.Domain/Extensions/IEnumerableExtensions.cs
+++ Library.Domain/Extensions/IEnumerableExtensions.cs
@@ -3,19 +3,20 @@
     using Library.Domain.Extensions.Helpers;
     using System;
     using System.Collections.Generic;
+    using System.Dynamic;
     using System.Reflection;
     using System.Text;
 
     public static class IEnumerableExtensions
     {
-        public static IEnumerable<ExpandableObject> ShapeData<TSource>(this IEnumerable<TSource> source, string fields)
+        public static IEnumerable<ExpandoObject> ShapeData<TSource>(this IEnumerable<TSource> source, string fields)
         {
             if (source == null)
             {
                 throw new ArgumentException("source");
             }
 
-            var result = new List<ExpandableObject>();
+            var result = new List<ExpandoObject>();
 
             // create a list with propertyInfo object on TSource. Reflection is expensive, so rather than doing it for each obj in the list, we do it once and reuse the results
             // after all part of the reflection is on the type of the object (TSource), not the instance
@@ -51,7 +52,7 @@
             // run through the source objects
             foreach (TSource sourceObject in source)
             {
-                var dataShapedObject = new ExpandableObject();
+                var dataShapedObject = new ExpandoObject();
 
                 foreach (var propertyInfo in propertyInfoList)
                 {
